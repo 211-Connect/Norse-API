89f71c349fcaa95027846f31dcec966ffa2da8ed
devcshort <13677134+devcshort@users.noreply.github.com>
2024-07-24 14:32:32 -0700

    Add workaround for buntime

--- src/lib/ElasticClient.ts
+++ src/lib/ElasticClient.ts
@@ -1,4 +1,4 @@
-import { Client } from '@elastic/elasticsearch';
+import { Client, HttpConnection } from '@elastic/elasticsearch';
 
 const ElasticClient = new Client({
   node: process.env.ELASTIC_NODE as string,
@@ -6,6 +6,7 @@ const ElasticClient = new Client({
     apiKey: process.env.ELASTIC_API_KEY as string,
   },
   maxRetries: 5,
+  Connection: HttpConnection,
 });
 
 export { ElasticClient };
