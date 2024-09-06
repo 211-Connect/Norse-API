eb4e61c3d3f3349df541f7de9a89740710d3e7cb
devcshort <13677134+devcshort@users.noreply.github.com>
2024-09-06 15:49:42 -0700

    feat: Update resource endpoint

--- src/resource/resource.service.ts
+++ src/resource/resource.service.ts
@@ -1,8 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { HeadersDto } from 'src/common/dto/headers.dto';
 import { Resource } from 'src/common/schemas/resource.schema';
-import { Model, Schema } from 'mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class ResourceService {
@@ -11,6 +11,37 @@ export class ResourceService {
   ) {}
 
   async findById(id: string, options: { headers: HeadersDto }) {
-    return this.resourceModel.findById(id);
+    const resource = await this.resourceModel.findById(id, {
+      noop: 0,
+      translations: {
+        $elemMatch: {
+          locale: options.headers['accept-language'],
+        },
+      },
+    });
+
+    // If the resource wasn't found, we should check to see if a redirect exists
+    // for this resource. If it does, we should redirect the user to the new
+    // resource.
+    // if (!resource) {
+    //   const redirect = await Redirect.findById(id);
+
+    //   if (redirect) {
+    //     cacheControl(res);
+    //     return res.status(404).json({
+    //       redirect: `/search/${redirect.newId}`,
+    //     });
+    //   }
+    // }
+
+    // If the resource wasn't found, or if there are no translations for the
+    // resource, we should return a 404.
+    if (!resource || resource.translations.length === 0)
+      throw new BadRequestException();
+
+    const newV = resource.toJSON() as any;
+    newV.translation = resource.translations[0];
+
+    return newV;
   }
 }
