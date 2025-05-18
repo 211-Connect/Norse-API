3698fdccd917671e0ba5a9f3d9255c8be701427c
devcshort <13677134+devcshort@users.noreply.github.com>
2025-05-17 18:38:00 -0700

    feat: allow single string or string array for taxonoy terms

--- src/taxonomy/dto/taxonomy-terms-query.dto.ts
+++ src/taxonomy/dto/taxonomy-terms-query.dto.ts
@@ -1,7 +1,10 @@
 import { z } from 'zod';
 
 export const taxonomyTermsQuerySchema = z.object({
-  terms: z.array(z.string()).default([]),
+  terms: z
+    .union([z.array(z.string()), z.string()])
+    .transform((val) => (typeof val === 'string' ? [val] : val))
+    .default([]),
 });
 
 export type TaxonomyTermsQueryDto = z.infer<typeof taxonomyTermsQuerySchema>;
