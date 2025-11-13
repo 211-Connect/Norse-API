dd2cea9e7d63d57c39ba04e838e656290f1750f7
Tomasz Fidecki <tomasz.fidecki@uninterrupted.tech>
2025-11-13 18:41:21 +0100

    feat: add CA certificate for OpenSearch SSL configuration and update file path in OpenSearch service

--- src/hybrid_semantic/services/opensearch.service.ts
+++ src/hybrid_semantic/services/opensearch.service.ts
@@ -9,6 +9,7 @@ import * as nlp from 'wink-nlp-utils';
 import winkNLP from 'wink-nlp';
 import model from 'wink-eng-lite-web-model';
 import { readFileSync } from 'fs';
+import { join } from 'path';
 
 /**
  * Service for building and executing OpenSearch queries
@@ -30,7 +31,9 @@ export class OpenSearchService {
       'http://localhost:9200';
     const nodeEnv = this.configService.get<string>('NODE_ENV') || 'development';
     // const caCert = this.configService.get<string>('CA_CERT_DB_OPENSEARCH');
-    const caCert = readFileSync('ca-cert-do-opensearch.pem');
+    const caCert = readFileSync(
+      join(__dirname, '../../../certs/ca-cert-do-opensearch.pem'),
+    );
 
     // Configure SSL based on environment
     const sslConfig =
