e7a3cc17b172f03a221b294a5677ff8c66b12fe5
Bartek Gałęzowski <bartlomiej.galezowski@u11d.com>
2026-03-17 13:11:22 +0100

    refactor: update Pushgateway type to use PrometheusContentType in MetricsService

--- src/metrics/metrics.service.ts
+++ src/metrics/metrics.service.ts
@@ -2,19 +2,18 @@ import { Injectable, Logger, OnModuleDestroy } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import {
   Counter,
+  PrometheusContentType,
   Pushgateway,
   collectDefaultMetrics,
   register,
 } from 'prom-client';
-
-type PUSHGATEWAY_CONFIG = 'text/plain; version=0.0.4; charset=utf-8';
 
 @Injectable()
 export class MetricsService implements OnModuleDestroy {
   private readonly logger = new Logger(MetricsService.name);
   private readonly searchHitsCounter: Counter;
   private readonly resourceHitsCounter: Counter;
-  private readonly gateway: Pushgateway<PUSHGATEWAY_CONFIG> | null;
+  private readonly gateway: Pushgateway<PrometheusContentType> | null;
   private readonly pushIntervalMs: number;
   private pushInterval: NodeJS.Timeout | null = null;
 
