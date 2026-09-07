a30cce8145ac1c44ab796fe123f33d67c9510931
Michał Miler <michal.miler@uninterrupted.tech>
2026-09-07 09:21:22 +0200

    fix suggestion module imports

--- src/suggestion/suggestion.module.ts
+++ src/suggestion/suggestion.module.ts
@@ -3,10 +3,11 @@ import { SuggestionController } from './suggestion.controller';
 import { SuggestionService } from './suggestion.service';
 import { OrganizationModule } from 'src/organization/organization.module';
 import { TaxonomyModule } from 'src/taxonomy/taxonomy.module';
+import { CmsConfigModule } from 'src/cms-config/cms-config.module';
 
 @Module({
   controllers: [SuggestionController],
   providers: [SuggestionService],
-  imports: [OrganizationModule, TaxonomyModule],
+  imports: [OrganizationModule, TaxonomyModule, CmsConfigModule],
 })
 export class SuggestionModule {}
